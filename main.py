1fd533e70e65c86adaee0fda149d1050e6ac47e8
Rahul G <thegr8rg99@gmail.com>
2022-10-15 09:15:33 -0700

    Update main.py
    adding opening door info to print

--- main.py
+++ main.py
@@ -27,15 +27,14 @@ while(i<n):
     
     opening_opts = list(set(doors) - set(car_door+select_door)) #possible doors to open such that its not the car-door or the player selected door
     opening = opening_opts[random.randint(0,len(opening_opts)-1)] #opening one dud-door
-    opening_door = doors[opening]
     
     #which option wins?
     if(car == select):
         stay_wins+=1
-        print("Iteration :",i," Selection:",select_door," Opening Door:",opening_door," Car is behind ",car_door," Stay wins")    
+        print("Iteration :",i," Selection:",select_door," Opening Door:",opening," Car is behind ",car_door," Stay wins")    
     else:
         switch_wins+=1
-        print("Iteration :",i," Selection:",select_door," Opening Door:",opening_door," Car is behind ",car_door,"Switch wins")
+        print("Iteration :",i," Selection:",select_door," Opening Door:",opening," Car is behind ",car_door,"Switch wins")
         
 win_p = stay_wins*100/n
 
